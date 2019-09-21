65544b7221384e4d7d9482d7cc68d30022546e19
Abel de Wit <abeldewit@gmail.com>
2019-09-21 01:42:40 +0000

    Welterusten

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
 // JavaScript Document
 const navSlide = () => {
 	const burger = document.querySelector('.burger');
+	const navLinks = document.querySelectorAll('.nav-links li');
+	
+	burger.addEventListener('click',() =>{
+		navBar();
+	});
+	
+	navLinks.forEach((link, index)=>{
+		link.addEventListener('click',()=>{
+			navBar();
+		});
+		navBar();
+	})
+	
+	
+}
+
+function navBar() {
+	const burger = document.querySelector('.burger');
 	const nav = document.querySelector('.nav-links');
 	const navLinks = document.querySelectorAll('.nav-links li');
 	const body = document.querySelector('.content');
+	body.classList.toggle('content-faded');
+	nav.classList.toggle('nav-active');
 	
-	burger.addEventListener('click',() =>{
-		body.classList.toggle('content-faded');
-		nav.classList.toggle('nav-active');
-		
-		
-		
-		navLinks.forEach((link, index)=>{
-			if(link.style.animation) {
-				link.style.animation = "";
-			} else {
-				link.style.animation = `navlinkFade 0.5s ease forwards ${index / 5 + 0.5}s`;
-			}
-		});
+	navLinks.forEach((link, index)=>{
+		if(link.style.animation) {
+			link.style.animation = "";
+		} else {
+			link.style.animation = `navlinkFade 0.5s ease forwards ${index / 5 + 0.5}s`;
+		}
 	});
-	
-	
 }
 
 navSlide();
